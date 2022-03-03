f1d6e5c3cbe471e2f0e5ca1e7623850581071aca
Chang-Hong Chen <longhongc@gmail.com>
2022-03-03 16:13:19 -0500

    Test create line
    create a line with 2 points
    uisng ax+by+d=0, (a,b) is the normal vector to the line

--- map.py
+++ map.py
@@ -42,6 +42,22 @@ class Map:
     @classmethod
     def is_obstacle(self, pos): 
         occupancy_grid_map = np.zeros((Map.width, Map.height))
+
+        # ax+by+d=0
+        def create_line(p1, p2):
+            assert(p1!=p2),"point1 equals to point2, cannot form line"
+
+            tangent_vector = (p2[0]-p1[0], p2[1]-p1[1])
+            if (tangenet_vector[0]==0):
+                normal_vector = (1,0)
+            elif (tangenet_vector[1]==0): 
+                normal_vector = (0,1)
+            else:
+                normal_vector = (1/(p2[0]-p1[0]), -1/(p2[1]-p1[1]))
+            a, b = normal_vector
+            d = -(a * p1[0] + b * p1[1])
+            return a, b, d
+
         #def form_obstacle()
         return False
 
@@ -49,37 +65,60 @@ class Map:
         pass
 
 def start_simulation(): 
-    success=False
+    success = False
     while (not success):
         # read input
         print("map width: 400, map height: 250")
         print("The robot is a circle with radius 5")
         print("Enter a start and goal point in the map")
 
-        start_x = input("Start x:")
-        start_y = input("Start y:")
+        start_x = int(input("Start x:"))
+        start_y = int(input("Start y:"))
         start = (start_x, start_y)
 
-        goal_x = input("Goal x:")
-        goal_y = input("Goal y:")
+        goal_x = int(input("Goal x:"))
+        goal_y = int(input("Goal y:"))
         goal = (goal_x, goal_y)
 
         print("Start pos: ({}, {})".format(start_x, start_y))
         print("Goal pos: ({}, {})".format(goal_x, goal_y))
 
-        if (not Map.is_obstacle(start)):
+        success = True
+
+        if (Map.is_obstacle(start)):
             print("This start point is not valid.")
             success = False
-        if (not Map.is_obstacle(goal)):
+        if (Map.is_obstacle(goal)):
             print("This goal point is not valid.")
             restart = False
         print("---")
 
     return start, goal
 
+# ax+by+d=0
+def create_line(p1, p2):
+    assert(p1!=p2),"point1 equals to point2, cannot form line"
+
+    tangent_vector = (p2[0]-p1[0], p2[1]-p1[1])
+    if (tangent_vector[0]==0):
+        normal_vector = (1,0)
+    elif (tangent_vector[1]==0): 
+        normal_vector = (0,1)
+    else:
+        normal_vector = (1/(p2[0]-p1[0]), -1/(p2[1]-p1[1]))
+    a, b = normal_vector
+    d = -(a * p1[0] + b * p1[1])
+    return a, b, d
+
 if __name__ == "__main__":
     start, goal = start_simulation()
     my_map = Map(start, goal)
+    a, b, d = create_line(start, goal)
+    print(a)
+    print(b)
+    print(d)
+    print(a*start[0]+b*start[1]+d)
+    print(a*goal[0]+b*goal[1]+d)
 
 
 
